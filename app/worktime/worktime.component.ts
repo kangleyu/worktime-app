ea4437ea13dbd90192d811a9e7b6942dcfc9d657
Tom Yu <kangleyu@gmail.com>
2017-06-04 21:58:57 +0800

    add worktype/worktime pages

--- app/worktime/worktime.component.ts
+++ app/worktime/worktime.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit
+} from '@angular/core';
+import { IWorktime } from '../shared/models/index';
+import {
+  Toastr,
+  TOASTR_TOKEN,
+  WorktimeService
+} from '../shared/index';
 
 @Component({
   selector: 'wt-worktime',
   templateUrl: './app/worktime/worktime.component.html'
 })
 export class WorktimeComponent {
+  worktimes: IWorktime[];
 
+  constructor(
+    private worktimeService: WorktimeService,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr) {
+  }
+
+  ngOnInit() {
+    this.worktimeService.getWorktimes().subscribe((worktimes) => {
+      this.worktimes = worktimes;
+    });
+  }
 }
--- app/worktype/worktype.component.ts
+++ app/worktype/worktype.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Inject,
+  OnInit
+} from '@angular/core';
+import { IWorktype } from '../shared/models/index';
+import {
+  Toastr,
+  TOASTR_TOKEN,
+  WorktypeService
+} from '../shared/index';
 
 @Component({
   selector: 'wt-worktype',
   templateUrl: './app/worktype/worktype.component.html'
 })
 export class WorktypeComponent {
+  worktypes: IWorktype[];
 
+  constructor(
+    private worktypeService: WorktypeService,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr) {
+  }
+
+  ngOnInit() {
+    this.worktypeService.getWorktypes().subscribe((worktypes) => {
+      this.worktypes = worktypes;
+    });
+  }
 }
