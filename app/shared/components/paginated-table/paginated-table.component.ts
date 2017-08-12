9dc16c3d8b9f71a24fa7ae8d60cf8d095bcce1e3
Tom Yu <kangleyu@gmail.com>
2017-08-12 11:23:48 +0800

    move the table field setting inside of component, we can pass fields list through property 'fields'

--- app/shared/components/paginated-table/paginated-table.component.ts
+++ app/shared/components/paginated-table/paginated-table.component.ts
@@ -1,16 +1,43 @@
 import {
   Component,
   Inject,
-  OnInit
+  OnChanges,
+  Input,
+  ElementRef
 } from '@angular/core';
 
 @Component({
   selector: 'paginated-table',
   templateUrl: './app/shared/components/paginated-table/paginated-table.component.html'
 })
-export class PaginatedTableComponent implements OnInit {
+export class PaginatedTableComponent implements OnChanges {
+  @Input() items: any;
+  @Input() fields: string;
 
-  ngOnInit() {
-    const caption = "test";
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnChanges() {
+    if(this.fields !== undefined &&
+      this.fields.length > 0 &&
+      this.items !== undefined &&
+      this.items.length > 0) {
+      const fieldsList = this.fields.split('|');
+      let inner = "<table class=\"table table-striped wt-table\">";
+      fieldsList.forEach((ele) => {
+        const separatorIndex = ele.indexOf(':');
+        inner += "<th data-field=\"" + ele.substr(0, separatorIndex) + "\">" + ele.substr(separatorIndex + 1) + "</th>";
+      });
+      inner += "<tbody>";
+      this.items.forEach((data) => {
+        inner += "<tr>";
+        fieldsList.forEach((ele) => {
+          inner += "<td>" + data[ele.substr(0, ele.indexOf(':'))] + "</td>";
+        });
+        inner +="</tr>";
+      });
+      inner += "</tbody></table>";
+      this.elementRef.nativeElement.innerHTML = inner;
+    }
   }
 }
