341020b83c50704a5199e9ab8af5293def6183c9
Tom Yu <kangleyu@gmail.com>
2017-09-10 15:28:58 +0800

    add field transform pipe for later reflectoring table component

---
+++ app/shared/pipes/fields.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform, Injectable } from "@angular/core";
+
+@Pipe({
+  name: "parseFields"
+})
+export class ParseFieldsPipe implements PipeTransform {
+  constructor() {
+  }
+
+  transform(value: any, pattern?: string): any {
+    console.log("PARSING...");
+    const ret = [];
+    if (value !== undefined) {
+      const fieldsList = value.split('|');
+      fieldsList.forEach((ele) => {
+        const separatorIndex = ele.indexOf(':');
+        const field = ele.substr(0, separatorIndex);
+        const display = ele.substr(separatorIndex + 1);
+        ret.push({ name: field, text: display });
+      });
+    }
+    return ret;
+  }
+}
