b0eba777215e6fd61832ff49f8c10115513a578f
Tom Yu <kangleyu@gmail.com>
2017-09-16 10:25:32 +0800

    mute html security warning for cell format, details - https://angular.io/api/platform-browser/DomSanitizer

--- app/shared/pipes/cellFormatter.pipe.ts
+++ app/shared/pipes/cellFormatter.pipe.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform, Injectable } from "@angular/core";
 import { DatePipe } from "@angular/common";
 import { MonthPipe, StatusPipe, VerifiedStatusPipe } from "../index";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
   name: "cellFormatter"
@@ -11,22 +12,23 @@ export class CellFormatterPipe implements PipeTransform {
     private month: MonthPipe,
     private status: StatusPipe,
     private date: DatePipe,
-    private verify: VerifiedStatusPipe) {
+    private verify: VerifiedStatusPipe,
+    private sanitizer: DomSanitizer) {
   }
 
   transform(value: any, pattern?: string): any {
     switch (pattern) {
       case "createdAt":
       case "updatedAt":
-        return this.date.transform(value, "yyyy-MM-dd");
+        return this.sanitizer.bypassSecurityTrustHtml(this.date.transform(value, "yyyy-MM-dd"));
       case "state":
-        return this.status.transform(value);
+        return this.sanitizer.bypassSecurityTrustHtml(this.status.transform(value));
       case "isUpperHalf":
-        return this.month.transform(value);
+        return this.sanitizer.bypassSecurityTrustHtml(this.month.transform(value));
       case "verified":
-        return this.verify.transform(value);
+        return this.sanitizer.bypassSecurityTrustHtml(this.verify.transform(value));
       default:
-        return value;
+        return this.sanitizer.bypassSecurityTrustHtml(value);
     }
   }
 }
