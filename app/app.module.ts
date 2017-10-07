740e4dd0fc90030cdf0e1284e6674bd0ed476a50
Tom Yu <kangleyu@gmail.com>
2017-10-07 21:36:46 +0800

    add page for 404 page

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,30 +1,99 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { ChartsModule } from 'ng2-charts';
+import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { ErrorModule } from "./error/error.module";
-import { MainModule } from "./main/main.module";
-import { LoginModule } from "./login/login.module";
+
+import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { EmployeeComponent } from "./employee/employee.component";
+import { PaymentComponent } from "./payment/payment.component";
+import { ProjectComponent } from "./project/project.component";
+import { WorktypeComponent } from "./worktype/worktype.component";
+import { WorktimeComponent } from "./worktime/worktime.component";
+import { StaticsComponent } from "./statics/statics.component";
+import { NotFoundComponent } from "./error/notFound.component";
+import { MainComponent } from './main/main.component';
+
+import { AuthGuard } from "./guards/index";
+
 import { AppComponent } from './app.component';
 
-// const appRoutes: Routes = [
-//   {
-//     path: '*', redirectTo: 'notFound'
-//   }
-// ];
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'app',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'employee',
+        component: EmployeeComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'payment',
+        component: PaymentComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'project',
+        component: ProjectComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'worktime',
+        component: WorktimeComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'worktype',
+        component: WorktypeComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'statics',
+        component: StaticsComponent,
+        canActivate: [AuthGuard]
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([]),
-    CoreModule,
-    MainModule,
-    LoginModule,
-    ErrorModule,
+    RouterModule.forRoot(appRoutes),
+    ChartsModule,
+    SharedModule,
+    CoreModule
   ],
   declarations: [
+    LoginComponent,
+    MainComponent,
+    DashboardComponent,
+    EmployeeComponent,
+    PaymentComponent,
+    ProjectComponent,
+    WorktimeComponent,
+    WorktypeComponent,
+    StaticsComponent,
+    NotFoundComponent,
     AppComponent
   ],
-  providers: [],
+  providers: [
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- app/error/notFound.component.ts
+++ app/error/notFound.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'not-found',
-  template: './app/error/notFound.component.html',
+  templateUrl: './app/error/notFound.component.html',
   styleUrls: ['./app/error/notFound.component.css']
 })
 export class NotFoundComponent {
